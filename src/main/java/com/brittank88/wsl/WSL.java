7d9951ed9c887c8a2f746aea966b777344f27670
Brittank88 <masonhunt130ii@gmail.com>
2022-02-21 11:30:04 +1100

    Logging fix

--- src/main/java/com/brittank88/wsl/WSL.java
+++ src/main/java/com/brittank88/wsl/WSL.java
@@ -1,13 +1,18 @@
 package com.brittank88.wsl;
 
 import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class WSL implements ModInitializer {
+
+    public static final Logger LOGGER = LogManager.getLogger("WSL");
+
     @Override
     public void onInitialize() {
-        System.out.println("Initializing WSL!");
-        System.out.println("Max player speed: " + 100.0F + " -> " + Float.MAX_VALUE);
-        System.out.println("Max elytra speed: " + 300.0F + " -> " + Float.MAX_VALUE);
-        System.out.println("Max vehicle speed: " + 100.0 + " -> " + Double.MAX_VALUE);
+        LOGGER.info("[Player] 100.0F -> " + Float.MAX_VALUE);
+        LOGGER.info("[Elytra] 300.0F -> " + Float.MAX_VALUE);
+        LOGGER.info("[Vehicle] 100.0 -> " + Double.MAX_VALUE);
+        LOGGER.info("WSL has been initialized!");
     }
 }
